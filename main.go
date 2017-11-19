a4956bcd9a312c2e1bcf186b8a89c37e9a23d09b
David Huang <dhuhuang@ucdavis.edu>
2017-11-19 15:42:33 -0800

    Add files via upload

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.185.33.148 :9200"
+	ES_URL = "http://104.197.35.223:9200"
 )
 
 
